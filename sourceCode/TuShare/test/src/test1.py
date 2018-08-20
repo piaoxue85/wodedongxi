02976ca2ea6165f15575823ac74f854983da7ed8
moonlit77 <p33306308@126.com>
2018-08-20 21:02:41 +0800

    微软

--- sourceCode/TuShare/test/src/test1.py
+++ sourceCode/TuShare/test/src/test1.py
@@ -1,10 +1,11 @@
-import numpy as np
-import pandas as pd
+# 23bb6cd2f77e39ea16ca656f2fc1ec2aaaeb6d84aed8369030be7ee8
+import tushare as ts
+print(ts.__version__)
+ts.set_token("23bb6cd2f77e39ea16ca656f2fc1ec2aaaeb6d84aed8369030be7ee8")
+pro = ts.pro_api()
 
-df = pd.DataFrame()
-df["a"] = [2,2,2,2,2]
-df["b"] = [2,2,2,2,2]
-df["c"] = [3,3,3,3,3]
-df["d"] = df["c"].values * (df["a"].values/df["b"].values)
+df = pro.adj_factor(ts_code='002230.SZ', trade_date='')
+print(df)
 
+df = pro.daily(ts_code='002230.SZ', start_date='', end_date='')
 print(df)
