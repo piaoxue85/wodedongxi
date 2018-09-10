bcf11beff3b96262849a011bc6871b9e5ac41db6
moonlit <p33306308@126.com>
2018-09-10 23:53:47 +0800

    送审

--- sourceCode/TuShare/test/src/test1.py
+++ sourceCode/TuShare/test/src/test1.py
@@ -1,4 +1,9 @@
 # 23bb6cd2f77e39ea16ca656f2fc1ec2aaaeb6d84aed8369030be7ee8
+
+from torch import randperm
+import autokeras as ak
+clf = ak.ImageClassifier()
+
 import tushare as ts
 print(ts.__version__)
 ts.set_token("23bb6cd2f77e39ea16ca656f2fc1ec2aaaeb6d84aed8369030be7ee8")
